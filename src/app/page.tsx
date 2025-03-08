a93a113047cc76dbd6dccccc6e48b40e33b0fabb
Antonio Marcos Souto <a.marcossouto08@gmail.com>
2025-03-08 18:28:59 -0300

    Create test branch with all biomes available for testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import About from './_components/About'
+import BiomesList from './_components/BiomesList'
 import Details from './_components/Details'
 import Hero from './_components/Hero'
 import Quote from './_components/Quote'
@@ -8,10 +9,11 @@ import Quote from './_components/Quote'
 export default function Home() {
   return (
     <div>
-        <Hero />
-        <Quote />
-        <About />
-        <Details />
+      <Hero />
+      <Quote />
+      <About />
+      <BiomesList />
+      <Details />
     </div>
   )
 }
